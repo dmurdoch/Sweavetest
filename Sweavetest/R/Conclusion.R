c6e5ae1246c7761c7bb85d4ae93abfd91f272a4e
Adam Rahman <arahma26@uwo.ca>
2013-01-18 18:59:59 -0500

    Changed to protect Index file from accidental deletion by user

--- Sweavetest/R/Conclusion.R
+++ Sweavetest/R/Conclusion.R
@@ -9,6 +9,16 @@ Conclusion <- function(){
     
     Appendix()
     
+    if(Version() == "Student"){
+      if(file.exists(paste0(TestName(), "Index.csv"))){
+        TestIndex <- read.csv(paste0(TestName(), "Index.csv"))
+        ExamCodes <- TestIndex$ExamCode
+        if(length(which(ExamCodes == versioncode())) > 0){
+          stop("Test version already exists in index file. Delete index file if re-running tests, or change test version.")
+        }      
+      }
+    }
+    
     if(Version() == "Student") {
         if(testversion() == 1)
           write.csv(Index(),paste0(TestName(), "Index.csv"), row.names = FALSE)
